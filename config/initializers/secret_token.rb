e409b6477718468593a708d773e6b203aa47696d
Pavel Osetrov <pavel.osetrov@me.com>
2014-06-04 17:45:27 +0400

    BSC

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bsc::Application.config.secret_key_base = 'da6d011cdcd1c70b9139664e23c2030333709c0847291d041e947ea8f80ed4c6855c3200414a0668cf20b24f42f7aecc118ebff774fc7a3557b86c2cf8674ff6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bsc::Application.config.session_store :cookie_store, key: '_bsc_session'
